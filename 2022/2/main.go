be3a9e4dae5e1c0881ca7d86479edfcfe6d5bf7f
davidjcmar <davidjcmar@gmail.com>
2022-12-08 21:48:28 -0500

    second half

--- 2022/2/main.go
+++ 2022/2/main.go
@@ -23,27 +23,52 @@ func main() {
 	}
 	scan := bufio.NewScanner(bufio.NewReader(f))
 	scan.Split(bufio.ScanWords)
-	var score int
+	var score, second_score int
 	for scan.Scan() {
 		opp := cipher[scan.Text()]
 		scan.Scan()
 		me := cipher[scan.Text()]
+		// first half
 		switch outcome := opp - me; {
-			// tie
+			// draw
 			case outcome == 0:
 				score += 3
 				score += me
-				fmt.Printf("Draw opp: %v  me: %v  outcome: %v\n", opp, me, outcome)
+				// fmt.Printf("Draw\t\tOpp: %v\t\tMe: %v\n", opp, me)
 			// win
 			case outcome == -1, outcome == 2:
 				score += 6
 				score += me
-				fmt.Printf("Win opp: %v  me: %v  outcome: %v\n", opp, me, outcome)
 			// loss
 			case outcome == 1, outcome == -2:
 				score += me
-				fmt.Printf("Loss opp: %v  me: %v  outcome: %v\n", opp, me, outcome)
+		}
+		// second half
+		switch (me) {
+			// loss
+			case 1:
+				x := (opp + 2) % 3
+				if x == 0 {
+					x = 3
+				}
+				second_score += x
+				fmt.Printf("Loss opp: %v  me: %v\n", opp, me)
+			// draw
+			case 2:
+			    second_score += opp
+				second_score += 3
+				fmt.Printf("Draw opp: %v  me: %v\n", opp, me)
+			// win
+			case 3:
+				x := (opp + 1) % 3
+				if x == 0 {
+					x = 3
+				}
+				second_score += x
+				second_score += 6
+				fmt.Printf("Win opp: %v  me: %v\n", opp, me)
 		}
 	}
 	fmt.Println(score)
+	fmt.Printf("Second score: %v\n", second_score)
 }
